ea47051beb2118e437efb4ca792b9dc7740c81b7
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-09-15 16:57:19 -0700

    adding functions

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-       num1: 1,
-       num2: 1
+        num1: 1,
+        num2: 1,
+        score: 0,
+        response: ""
     }
   }
 
@@ -15,9 +17,17 @@ class App extends React.Component {
   render() {
     return(
       <div>
-
+      <h1>{this.state.num1} + {this.state.num2}</h1> 
+       <input onChange={this.updateResponse} value={this.state.response } />
+       Current guess is {this.state.response}
       </div>
     );
+  }
+
+  updateResponse = (event) => {
+    this.setState({
+      response: event.target.value
+    });
   }
 }
 
